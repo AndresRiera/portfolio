8f9a958897e2ffc8260eda3229cdcccf7565e026
AndresRiera <arieraortizparraga@ucsd.edu>
2025-01-22 14:39:24 -0800

    nav

---
+++ global.js
@@ -0,0 +1,23 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume.html', title: 'Resume' },
+    { url: 'https://github.com/AndresRiera', title: 'GitHub' },
+  ];
+
+  let nav = document.createElement('nav');
+  document.body.prepend(nav);
+
+  for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
