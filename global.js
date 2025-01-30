49fe04ea12d303cb6d4974e377549e01b96ba4b4
AndresRiera <arieraortizparraga@ucsd.edu>
2025-01-30 13:58:05 -0800

    json update

--- global.js
+++ global.js
@@ -93,3 +93,40 @@ let pages = [
 
     location.href = url;
     });
+
+    export async function fetchJSON(url) {
+      try {
+          const response = await fetch(url);
+  
+          if (!response.ok) {
+              throw new Error(`Failed to fetch projects: ${response.statusText}`);
+          }
+  
+          const data = await response.json();
+          return data;
+      } catch (error) {
+          console.error('Error fetching or parsing JSON data:', error);
+      }
+  }
+
+  import { fetchJSON } from './global.js';
+
+async function displayProjects() {
+    const projectsContainer = document.querySelector('.projects');
+
+    const projects = await fetchJSON('../lib/projects.json');
+
+  
+    projects.forEach(({ title, image, description }) => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+            <h2>${title}</h2>
+            <img src="${image}" alt="${title}">
+            <p>${description}</p>
+        `;
+        projectsContainer.appendChild(article);
+    });
+}
+
+
+displayProjects();
