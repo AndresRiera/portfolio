b4cee6450ebceb50420c8f41b228028a5a497e0f
AndresRiera <arieraortizparraga@ucsd.edu>
2025-01-31 14:31:01 -0800

    lab4.6

--- Projects/projects.js
+++ Projects/projects.js
@@ -24,21 +24,22 @@
 //     }
 // })();
 
+//////////////////////////////
 
-import { fetchJSON, renderProjects } from '../global.js';
+// import { fetchJSON, renderProjects } from '../global.js';
 
-(async () => {
-    const projects = await fetchJSON('../lib/projects.json');
+// (async () => {
+//     const projects = await fetchJSON('../lib/projects.json');
 
-    const projectsTitle = document.querySelector('.projects-title');
-    projectsTitle.textContent = `Projects (${projects.length})`;
+//     const projectsTitle = document.querySelector('.projects-title');
+//     projectsTitle.textContent = `Projects (${projects.length})`;
 
-    const projectsContainer = document.querySelector('.projects');
-    if (projects && projects.length > 0) {
-        projects.forEach((project) => {
-            renderProjects(project, projectsContainer, 'h2');
-        });
-    } else {
-        projectsContainer.innerHTML = '<p>No projects to display at the moment.</p>';
-    }
-})();
+//     const projectsContainer = document.querySelector('.projects');
+//     if (projects && projects.length > 0) {
+//         projects.forEach((project) => {
+//             renderProjects(project, projectsContainer, 'h2');
+//         });
+//     } else {
+//         projectsContainer.innerHTML = '<p>No projects to display at the moment.</p>';
+//     }
+// })();
